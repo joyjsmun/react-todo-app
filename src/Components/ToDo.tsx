54d7c0706fa37927e6c686a2d32e904824d307b8
joyjsmun <joy.js.mun@gmail.com>
2022-03-07 17:58:15 -0800

    ADD: Categories

--- src/Components/ToDo.tsx
+++ src/Components/ToDo.tsx
@@ -1,11 +1,20 @@
-import { IToDo } from "./atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "./atoms";
 
-function ToDo({text}:IToDo){
+function ToDo({text,category}:IToDo){
+    const setToDos = useSetRecoilState(toDoState);
+    const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {
+        const {
+            currentTarget:{name}
+        } = event;
+    };
+   
     return (<li>
         <span>{text}</span>
-        <button>To Do</button>
-        <button>Doing</button>
-        <button>Done</button>
+        {category !== "TO_DO" && <button name="TO_DO" onClick={onClick}>TO_DO</button>}
+        {category !== "DOING" && <button name="DOING" onClick={onClick}>DOING</button>}
+        {category !== "DONE" && <button name="DONE" onClick={onClick}>DONE</button>}
 
     </li>);
 }
