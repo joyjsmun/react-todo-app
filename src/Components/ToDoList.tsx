a0f7f91234cee608e189b4c8a6c6642c61f20b0e
joyjsmun <joy.js.mun@gmail.com>
2022-03-07 17:31:15 -0800

    ADD: refactoring

--- src/Components/ToDoList.tsx
+++ src/Components/ToDoList.tsx
@@ -1,46 +1,23 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import {atom,useRecoilState} from "recoil";
+
+import { useRecoilValue } from "recoil";
+import { toDoState } from "./atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
 
 
-
-interface IForm {
-  toDo:string;
-}
-
-interface IToDo{
-  text:string;
-  id:number;
-  category:"TO_DO" | "DOING" | "DONE";
-}
-
-const toDosState = atom<IToDo[]>({
-  key:"toDo",
-  default:[],
-})
 function ToDoList() {
   //useRecoilValue; connect with atoms + useSetRecoilState;set Atoms
-  const [toDos,setToDos] = useRecoilState(toDosState);
-  const {register,handleSubmit,setValue} = useForm<IForm>();
-  const handleValid = ({toDo}:IForm) => {
-    console.log("Add to do",toDo);
-    // {new Todo} ... old todos array
-    setToDos((oldToDos) => [{text:toDo, id:Date.now() ,category:"TO_DO"},...oldToDos]);
-    setValue("toDo","");
-  }
+  const toDos = useRecoilValue(toDoState);
+  //data.toDo => ({toDo}) 
+
   console.log(toDos);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
-        <form onSubmit={handleSubmit(handleValid)}>
-            <input {...register("toDo",{
-                required:"Please Write a Todo"
-            })} placeholder="Write a ToDo" />
-            <button>Add</button>
-        </form>
+     <CreateToDo />
       <ul>
-        {toDos.map(toDo => <li key={toDo.id}>{toDo.text}</li>)}
+        {toDos.map(toDo => <ToDo key={toDo.id} {...toDo}/> )}
       </ul>
     </div>
   );
